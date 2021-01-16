81244eca59bcc104542e2cdebe9e3f92dc180ec4
jviviand <viviandjeremy@gmail.com>
2021-01-16 17:18:40 +0100

    P4Instagrid

--- P4Instagrid/ViewController.swift
+++ P4Instagrid/ViewController.swift
@@ -28,46 +28,49 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var lastBouton: UIButton!
     @IBOutlet weak var lastImageView: UIImageView!
     
+    private var buttonImage: UIButton?
     
-    override func viewDidLoad() {
+        override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    @IBAction func firstHiddenBouton(_ sender: Any) {
-        firstImageView.isHidden = false
-        centerImageView.isHidden = true
-        lastImageView.isHidden = true
-        
-        ButtonPictureHautG.isHidden = false
-        buttonPictureHautD.isHidden = true
-        buttonPictureBasG.isHidden = false
-        buttonPictureBasD.isHidden = false
-
+    @IBAction func choiceButtonHidden(_ sender: UIButton) {
+        switch sender {
+        case firstBouton:
+            firstImageView.isHidden = false
+            centerImageView.isHidden = true
+            lastImageView.isHidden = true
+            
+            ButtonPictureHautG.isHidden = false
+            buttonPictureHautD.isHidden = true
+            buttonPictureBasG.isHidden = false
+            buttonPictureBasD.isHidden = false
+        case centerBouton:
+            firstImageView.isHidden = true
+            centerImageView.isHidden = false
+            lastImageView.isHidden = true
+            
+            ButtonPictureHautG.isHidden = false
+            buttonPictureHautD.isHidden = false
+            buttonPictureBasG.isHidden = false
+            buttonPictureBasD.isHidden = true
+        case lastBouton:
+            firstImageView.isHidden = true
+            centerImageView.isHidden = true
+            lastImageView.isHidden = false
+            
+            ButtonPictureHautG.isHidden = false
+            buttonPictureHautD.isHidden = false
+            buttonPictureBasG.isHidden = false
+            buttonPictureBasD.isHidden = false
+        default:
+            break
+        }
     }
     
-    @IBAction func centerHiddenBouton(_ sender: Any) {
-        firstImageView.isHidden = true
-        centerImageView.isHidden = false
-        lastImageView.isHidden = true
-        
-        ButtonPictureHautG.isHidden = false
-        buttonPictureHautD.isHidden = false
-        buttonPictureBasG.isHidden = false
-        buttonPictureBasD.isHidden = true
-        
-    }
-    @IBAction func lastHiddenBouton(_ sender: Any) {
-        firstImageView.isHidden = true
-        centerImageView.isHidden = true
-        lastImageView.isHidden = false
-        
-        ButtonPictureHautG.isHidden = false
-        buttonPictureHautD.isHidden = false
-        buttonPictureBasG.isHidden = false
-        buttonPictureBasD.isHidden = false
-    }
     
-    @IBAction func openPhotoLibraryButton(_ sender: Any) {
+       @IBAction func buttonPhotoLibrary(_ sender: UIButton) {
+        buttonImage = sender
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
             let Picker = UIImagePickerController()
             Picker.delegate = self
@@ -75,11 +78,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             Picker.allowsEditing = true
             present(Picker, animated: true, completion: nil)
         }
+
     }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
-        
-        dismiss(animated:true, completion: nil)
+        self.buttonImage?.setImage(image, for: .normal)
+        self.dismiss(animated: true, completion: nil)
     }
 }
 
